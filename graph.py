2fb290ca0bbb730aa76a165525d9c59fc207dbb5
Jonathan Keelan <sock.uk@gmail.com>
2014-11-26 11:18:23 +0000

    Added startcrawl

--- graph.py
+++ graph.py
@@ -11,23 +11,27 @@ class graph:
     def __init__(self,url):
         self.nodes = []
         self.urls = []
-        self.origurl = url
+        self.origin = url
 
     def addUrl(self,url,children):
         self.urls.append(url)
         self.nodes.append(node(url,children))
 
     def visitUrl(self,url):
-        children = ug.getUrls(url,self.origurl)
+        children = ug.getUrls(url,self.origin)
         if not url in self.urls:
             self.addUrl(url,children)
 
         for curl in children:
             if not curl in self.urls:
                 self.visitUrl(curl)
+
+    def startCrawl(self):
+        self.visitUrl(self.origin)
+        
 url = 'http://physics.open.ac.uk/~jphague/'
 f = graph(url)
-f.visitUrl(url)
+f.startCrawl()
 print f.urls
         
 
